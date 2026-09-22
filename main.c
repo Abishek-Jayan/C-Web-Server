673112a0a420de372ae87f589d54952c7c693cc3
Abishek Jayan <abishekjayan98@gmail.com>
2026-09-22 10:43:55 -0500

    fix:Learned about how to include WinSock32 Library and why its needed

--- main.c
+++ main.c
@@ -9,18 +9,31 @@ Goal and Functionality: Self explanatory
 Steps:-
 1. Set up CONSTANTS for IP ADDRESS and DEFAULT PORT
 2. Set up a server on IP ADDRESS and DEFAULT PORT displaying hello world.
-    2.1 
-3. Ensure HTTP Encryption for accessing localhost client.
-
+    2.1  How does the C program as the OS to setup a socket?
+3. How to bind IP ADDRESS AND DEFAULT PORT to socket?
+4. Listen for incoming connection requests
+4. Ensure HTTP Encryption for accessing localhost client.
+    4.1 Read the incoming request and check its header for HTTP
+    4.2 If yes, construct HTTP response based on request info
+    4.3 Send back the HTTP Response
+5. Close the connection after sending.(For now)
 
 What is a server?
 1. A server is a program that constantly monitors a port for incoming connections
-2. Socket: it is a combination of source IP + Port and destination IP + port and facilitates communication between client and server
+2. Socket: it is an integer provided by the OS to act as a communication endpoint. YOu bind the IP Address + Port to that integer.
+   2.1 Types of sockets: TCP, UDP
+3. Figure out how to get a tcp socket from the OS using C
+    3.1 Since my OS is Windows, I need to import the Winsock library to use the socket function. If this was Linux I can just use the socket command directly.
 */
+
+#include <winsock2.h>
+
+#pragma comment(lib,"Ws2_32.lib")
 
 const char IP_ADDRESS = "localhost";
 const char PORT = "8080";
 
 int main() {
+
     return 0;
 }
